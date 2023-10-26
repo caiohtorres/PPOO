147036eebf7f99010c91528bdd1c84c56854305f
Caio Torres <118904613+caiohtorres@users.noreply.github.com>
2023-10-26 15:02:39 -0300

    Delete AgendaContatos directory

--- AgendaContatos/src/PessoaFisica.java
+++
@@ -1,9 +0,0 @@
-public class PessoaFisica extends Pessoa{
-    private String cpf, estadoCivil;
-
-    public PessoaFisica(String nome, String endereco, String email, String cpf, String estadoCivil){
-        super(nome, endereco, email);
-        this.cpf = cpf;
-        this.estadoCivil = estadoCivil;
-    }
-}
